f026d9882d04d242915ca496eb04e0349c38f33c
merck-web <alekseyt.ognevo@mail.ru>
2023-06-18 10:56:44 +0500

    Добавил swager

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MoviesModule } from './movies/movies.module';
 
+
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/movie-db'),
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { Module } from '@nestjs/common';
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // ... Другой код конфигурации приложения
+
+  const options = new DocumentBuilder()
+    .setTitle('Movie API')
+    .setDescription('API for managing movies')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
